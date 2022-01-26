fb65a164eda55d08c41f83ed9a3420df5c016ba3
Marc Boreu <marcboreu@gmail.com>
2022-01-26 19:49:47 +0100

    first commit, update template project from Xcode

---
+++ Shared/Navigation_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_ControllerApp.swift
+//  Shared
+//
+//  Created by Marc Boreu on 26/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
